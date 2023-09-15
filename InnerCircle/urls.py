b3da92dcddaef02d3bf53b3d7a09dee6b05732ea
Pretty Pandey <prettypandeypearl@gmail.com>
2023-09-15 20:24:29 +0200

    User Profile Front end

--- InnerCircle/urls.py
+++ InnerCircle/urls.py
@@ -27,4 +27,6 @@ urlpatterns = [
 
 # Serve static files during development
 if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    
