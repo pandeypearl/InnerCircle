ac1b770c0d2d8ac445434ef45d5fe63514cc98b9
Pretty Pandey <prettypandeypearl@gmail.com>
2023-10-28 16:52:34 +0200

    Form validation for EventForm, UpdateEventForm, RSVPForm

--- events/forms.py
+++ events/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.forms import DateTimeInput
 from .models import Event, RSVP
+from django.utils import timezone
 
 class EventForm(forms.ModelForm):
     class Meta:
@@ -27,6 +28,24 @@ class EventForm(forms.ModelForm):
             'event_status': forms.Select(attrs={'class': 'form-select', 'placeholder': 'Event Status'}),
         }
 
+    def clean_date(self):
+        ''' Field level validation to ensure event date is in the future. '''
+        date = self.cleaned_data.get('date')
+        if date and date < timezone.now():
+            raise forms.ValidationError("Event date should be in the future.")
+        return date
+    
+    def clean(self):
+        ''' Form level validation ensuring that event_status only set if event is not a draft. '''
+        cleaned_data = super().clean()
+        event_status = cleaned_data.get('event_status')
+        is_draft = cleaned_data.get('is_draft')
+
+        if not is_draft and not event_status:
+            raise forms.ValidationError("Event Status is required if not a draft")
+        
+        return cleaned_data
+    
 
 class UpdateEventForm(forms.ModelForm):
     template_name = 'events/update_event.html'
@@ -69,6 +88,13 @@ class UpdateEventForm(forms.ModelForm):
             self.fields['event_status'].initial = instance.event_status
             self.fields['is_draft'].initial = instance.is_draft
 
+    def clean_date(self):
+        ''' Field level validation to ensure event date is in the future. '''
+        date = self.cleaned_data.get('date')
+        if date and date < timezone.now():
+            raise forms.ValidationError("Event date should be in the future.")
+        return date
+    
 
 class RSVPForm(forms.ModelForm):
     class Meta:
@@ -81,3 +107,10 @@ class RSVPForm(forms.ModelForm):
             'response_status': forms.Select(attrs={'class': 'form-select', 'placeholder': 'RSVP Response'}),
             'dietary_preferences': forms.Textarea(attrs={'rows': 5, 'class': 'form-control', 'placeholder': 'Dietary Preferences'}),
         }
+
+    def clean_dietary_preferences(self):
+        ''' Field level validation ensuring dietary preferences is not empty. '''
+        dietary_preferences = self.cleaned_data.get('dietary_preferences')
+        if not dietary_preferences:
+            raise forms.ValidationError("Dietary Preferences cannot be empty.")
+        return dietary_preferences
