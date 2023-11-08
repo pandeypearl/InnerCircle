6229ea6753e568de77d9b6f4145d4c84481368f0
Pretty Pandey <prettypandeypearl@gmail.com>
2023-11-08 17:18:51 +0200

    mobile nav for authenticated users and pages

---
+++ static/js/main.js
@@ -0,0 +1,7 @@
+function openNavbar() {
+    document.getElementById("mobileNavbar").style.display = "flex";
+}
+
+function closeNavbar() {
+    document.getElementById("mobileNavbar").style.display = "none";
+}
---
+++ staticfiles/js/main.js
@@ -0,0 +1,7 @@
+function openNavbar() {
+    document.getElementById("navbar").style.display = "flex";
+}
+
+function closeNavbar() {
+    document.getElementById("navbar").style.display = "none";
+}
